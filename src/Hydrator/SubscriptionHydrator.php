cca697516315956ce78533f4f0fed6c9e4902ca4
ppaulis <pascal.paulis@continuousphp.com>
2015-07-16 13:32:41 +0200

    added object hydration to fetchAll()

---
+++ src/Hydrator/SubscriptionHydrator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Zoho\Subscriptions\Hydrator;
+
+use Zend\Stdlib\Hydrator\ClassMethods as ClassMethodsHydrator;
+use Zoho\Subscriptions\Hydrator\Strategy\DateTimeFormatterStrategy;
+
+class SubscriptionHydrator extends ClassMethodsHydrator
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        error_log('test');
+
+        $datetimeStrategy = new DateTimeFormatterStrategy(\DateTime::ISO8601);
+        $this->addStrategy('created_time', $datetimeStrategy);
+        $this->addStrategy('updated_time', $datetimeStrategy);
+    }
+}
